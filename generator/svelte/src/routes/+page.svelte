cdf6aad3843174390b6886638802c6042c1c3c50
Fayez Nazzal <soulldev@gmail.com>
2023-10-13 07:43:13 +0300

    Add form for subscription to masjids

--- generator/svelte/src/routes/+page.svelte
+++ generator/svelte/src/routes/+page.svelte
@@ -5,6 +5,10 @@
 	import entries from 'lodash/entries';
 	import { browser } from '$app/environment';
 	import { APP_NAME } from '$lib/constants';
+	import SideOver from '$lib/components/SideOver.svelte';
+	import { env } from '$env/dynamic/public';
+	import { enhance } from '$app/forms';
+	import Spacer from '$lib/components/Spacer.svelte';
 
 	// This data object is the one returned by the load function
 	// wait we didn't assign it to anything, how does it work?
@@ -18,6 +22,9 @@
 		if (!search) return true;
 		return name.toLowerCase().includes(search.toLowerCase());
 	});
+
+	const SUBSCRIPTION_SIDEOVER_ID = 'subscription-sideover';
+	const SUBSCRIPTIONS_BASE_URL = env.PUBLIC_SUBSCRIPTIONS_BASE_URL;
 </script>
 
 <svelte:head>
@@ -25,48 +32,102 @@
 	<meta name="description" content="Prayer times for various masjids in tabular format" />
 </svelte:head>
 
-<div class="mx-auto prose max-w-6xl py-12 px-6 lg:px-8">
-	<h1>Masjid Prayer Times</h1>
-	<p>
-		View prayer times for various masjids below. To find a specific masjid, use the search bar at
-		the top.
-	</p>
+<SideOver id={SUBSCRIPTION_SIDEOVER_ID} close-aria-label="close subscriptions sideover">
+	<div slot="page" class="mx-auto prose max-w-6xl py-12 px-6 lg:px-8">
+		<h1>Masjid Prayer Times</h1>
+		<p>
+			View prayer times for various masjids below. To find a specific masjid, use the search bar at
+			the top.
+		</p>
 
-	<div class="w-full capitalize">
-		{#each masjids as [name, { iqamas, jumas }], i}
-			<h2>{name}</h2>
-			<div class="overflow-x-auto px-4">
-				<table class="table table-zebra">
-					<thead>
-						<tr>
-							<th>Iqama</th>
-							<th>Time</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each entries(iqamas) as [iqama, { time }]}
+		<div class="w-full capitalize">
+			{#each masjids as [name, { iqamas, jumas }], i}
+				<div class="flex items-center justify-between">
+					<h2>{name}</h2>
+
+					<label for={SUBSCRIPTION_SIDEOVER_ID} class="btn btn-primary drawer-button">
+						Subscribe
+					</label>
+				</div>
+				<div class="overflow-x-auto px-4">
+					<table class="table table-zebra">
+						<thead>
 							<tr>
-								<td>{iqama}</td>
-								<td>{time}</td>
+								<th>Iqama</th>
+								<th>Time</th>
 							</tr>
+						</thead>
+						<tbody>
+							{#each entries(iqamas) as [iqama, { time }]}
+								<tr>
+									<td>{iqama}</td>
+									<td>{time}</td>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+					<h3>Jumas</h3>
+					<ul>
+						{#each jumas as juma}
+							<li>
+								{juma}
+							</li>
 						{/each}
-					</tbody>
-				</table>
-				<h3>Jumas</h3>
-				<ul>
-					{#each jumas as juma}
-						<li>
-							{juma}
-						</li>
-					{/each}
-				</ul>
-			</div>
-			{#if i !== masjids.length - 1}
-				<Divider />
-			{/if}
-		{/each}
+					</ul>
+				</div>
+				{#if i !== masjids.length - 1}
+					<Divider />
+				{/if}
+			{/each}
+		</div>
 	</div>
-</div>
+
+	<form
+		slot="side"
+		class="flex flex-col flex-grow min-h-0 px-2 py-4 prose prose-li:my-0 prose-ul:px-0"
+		method="GET"
+		action={SUBSCRIPTIONS_BASE_URL}
+		use:enhance
+	>
+		<h2>Subscribe to Masjid Prayer Times</h2>
+
+		<div class="form-control w-full">
+			<label for="email" class="label">
+				<span class="label-text text-base font-medium">What is your email?</span>
+			</label>
+			<input
+				type="email"
+				name="email"
+				id="email"
+				placeholder="Type your email here"
+				class="input input-bordered w-full"
+			/>
+		</div>
+		<Spacer size="lg" />
+		<label for="topics" class="label text-base font-medium">
+			<span>Which masjids do you want to subscribe to?</span>
+		</label>
+		<ul class="w-full mt-1">
+			{#each masjids as [name]}
+				<li>
+					<label class="label justify-start cursor-pointer my-1">
+						<input type="checkbox" name="topics" class="checkbox checkbox-primary" value={name} />
+						<span class="label-text">{name}</span>
+					</label>
+				</li>
+			{/each}
+		</ul>
+		<div class="mt-auto flex items-center gap-4">
+			<button class="btn btn-primary flex-[0.8]" type="submit"> Subscribe </button>
+			<label
+				for={SUBSCRIPTION_SIDEOVER_ID}
+				class="flex-[0.2] btn btn-outline btn-neutral drawer-button"
+			>
+				Close
+			</label>
+		</div>
+	</form>
+</SideOver>
 
 <style>
 	.prose {
