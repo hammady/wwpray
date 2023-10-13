6ea6baf6454053cb8be58c728aaee25df2a32a2a
Fayez Nazzal <soulldev@gmail.com>
2023-10-13 07:42:41 +0300

    Add Spacer component

---
+++ generator/svelte/src/lib/components/Spacer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	const SIZES = {
+		x: {
+			sm: 'w-1',
+			md: 'w-2',
+			lg: 'w-3',
+			xl: 'w-4'
+		},
+		y: {
+			sm: 'h-1',
+			md: 'h-2',
+			lg: 'h-3',
+			xl: 'h-4'
+		}
+	} as const;
+
+	export let size: keyof (typeof SIZES)['y'] = 'sm';
+	export let axis: keyof typeof SIZES = 'y';
+</script>
+
+<div class={SIZES[axis][size]} />
