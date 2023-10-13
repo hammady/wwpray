2525b09f4d2ee6ca33dfaf0ca2291193894f3e8b
Fayez Nazzal <soulldev@gmail.com>
2023-10-13 07:42:35 +0300

    Add SideOver component

---
+++ generator/svelte/src/lib/components/SideOver.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let { id, ...props } = $$props as {
+		id: string;
+		'close-aria-label': string;
+	};
+</script>
+
+<div class="drawer drawer-end">
+	<input {id} type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<slot name="page" />
+	</div>
+	<div class="drawer-side z-[100]">
+		<label for={id} aria-label={props['close-aria-label']} class="drawer-overlay" />
+		<div
+			class="relative menu p-4 w-[clamp(320px,90vw,420px)] min-h-full bg-base-200 text-base-content"
+		>
+			<slot name="side" />
+		</div>
+	</div>
+</div>
