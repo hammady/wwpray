a11d34196db94a7fcb4f6fadb30fe248e1207810
Hossam Hammady <github@hammady.net>
2023-10-12 23:13:49 -0400

    Generate svelte files in a writable dir

--- generator/generate.js
+++ generator/generate.js
@@ -2,20 +2,25 @@ module.exports = async (dataFilePath, lastUpdatedFilePath, destinationDir) => {
     const fs = require('fs')
     const { execSync } = require('child_process')
     const svelteDir = './svelte'
+    const writableSvelteDir = '/tmp/svelte'
+
+    // Create destination directory and writable Svelte directory
+    fs.mkdirSync(destinationDir, { recursive: true })
+    fs.mkdirSync(writableSvelteDir, { recursive: true })
+
+    // Copy Svlete directory to a writable location
+    fs.cpSync(svelteDir, writableSvelteDir, { recursive: true })
     
     // Copy data files to be used by Svelte
-    const notifiedDestinationPath = svelteDir + '/src/routes/notified.json'
-    const lastUpdatedDestinationPath = svelteDir + '/src/routes/last_updated.txt'
-
-    fs.cpSync(dataFilePath, notifiedDestinationPath)
-    fs.cpSync(lastUpdatedFilePath, lastUpdatedDestinationPath)
+    fs.cpSync(dataFilePath, writableSvelteDir + '/src/routes/notified.json')
+    fs.cpSync(lastUpdatedFilePath, writableSvelteDir + '/src/routes/last_updated.txt')
     
     // Generate static site using Svelte
-    execSync('npm install', { cwd: svelteDir, stdio: 'inherit' })
-    execSync('npm run build', { cwd: svelteDir, stdio: 'inherit' })
+    execSync('npm install', { cwd: writableSvelteDir, stdio: 'inherit' })
+    execSync('npm run build', { cwd: writableSvelteDir, stdio: 'inherit' })
 
-    const svelteBuildDir = svelteDir + '/build'
-    fs.mkdirSync(destinationDir, { recursive: true })
+    // Copy necessary static files to destination directory
+    const svelteBuildDir = writableSvelteDir + '/build'
     for (const file of ['/_app', '/index.html', '/favicon.png']) {
         fs.cpSync(svelteBuildDir + file, destinationDir + file, { recursive: true })
     }
