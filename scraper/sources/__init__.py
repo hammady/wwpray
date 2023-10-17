e6c4617f3d0d49eb14b1133b0ced9004fc315340
Hossam Hammady <github@hammady.net>
2023-10-16 22:41:09 -0400

    Add new masjid: ICCO

--- scraper/sources/__init__.py
+++ scraper/sources/__init__.py
@@ -1,2 +1,3 @@
 from .mnn import MNNexusSource
 from .shalimar import ShalimarSource
+from .icco import ICCOSource
---
+++ scraper/sources/icco.py
@@ -0,0 +1,39 @@
+from .base import Source
+from bs4 import BeautifulSoup
+
+class ICCOSource(Source):
+    def __init__(self):
+        super().__init__("ICCO", headers={
+            "Accept": "text/html",
+        }, url="https://centres.macnet.ca/icco/")
+        
+    def parse(self):
+        if self._response is None:
+            raise Exception("No response set for source: " + self.name)
+        soup = BeautifulSoup(self._response.text, "html.parser")
+
+        nested_rows = soup.select("#dailyprayertime-2 > table > tr:nth-child(4) tr")
+
+        iqamas = {
+            "fajr": {
+                "time": soup.select_one("#dailyprayertime-2 > table > tr:nth-child(3) > td.jamah").text.strip()
+            },
+            "zuhr": {
+                "time": nested_rows[0].select_one("td.jamah").text.strip()
+            },
+            "asr": {
+                "time": nested_rows[1].select_one("td.jamah").text.strip()
+            },
+            "maghrib": {
+                "time": nested_rows[2].select_one("td.jamah").text.strip()
+            },
+            "isha": {
+                "time": nested_rows[3].select_one("td.jamah").text.strip()
+            },
+        }
+        # Get jumaa table rows and remove first and last rows
+        jumas = [f"{juma.text.strip()}" for juma in soup.select("#text-6 > div > p")][1:-1]
+        # Combine each tow rows into one
+        jumas = [jumas[i] + " by " + jumas[i+1] for i in range(0, len(jumas), 2)]
+
+        return iqamas, jumas
