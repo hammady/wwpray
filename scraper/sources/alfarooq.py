4219bc421b09aca3bae34825b16ade22a3001117
Hossam Hammady <github@hammady.net>
2023-10-20 07:21:16 -0400

    Fix juma extraction on Friday at Alfarooq

--- scraper/sources/alfarooq.py
+++ scraper/sources/alfarooq.py
@@ -12,12 +12,26 @@ class AlfarooqSource(HTMLSource):
 
         nested_rows = soup.select("table.dptTimetable > tr:nth-child(4) > tr")
 
+        if len(nested_rows) == 5:
+            # today is not Friday, jumas will be listed in the 5th row
+            jumas_row = 4
+        else:
+            # today is Friday, jumas will be listed instead of Zuhr
+            jumas_row = 0
+
+        spans = nested_rows[jumas_row].select("td.jamah > span")
+
+        if jumas_row == 4:
+            zuhr_tag = nested_rows[0].select_one("td.jamah")
+        else:
+            zuhr_tag = spans[0]
+
         iqamas = {
             "fajr": {
                 "time": soup.select_one("table.dptTimetable > tr:nth-child(3) > td.jamah").text.strip()
             },
             "zuhr": {
-                "time": nested_rows[0].select_one("td.jamah").text.strip()
+                "time": zuhr_tag.text.strip()
             },
             "asr": {
                 "time": nested_rows[1].select_one("td.jamah").text.strip()
@@ -31,8 +45,7 @@ class AlfarooqSource(HTMLSource):
         }
 
         jumas = [f"{label} Prayer: {value.text.strip()}" for value, label in zip(
-            nested_rows[4].select("td.jamah > span"),
-            self._counter_labels
+            spans, self._counter_labels
         )]
 
         return iqamas, jumas
