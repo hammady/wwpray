cf10261201adf4a0a6fa9138978f7a3002d326e2
Fayez Nazzal <soulldev@gmail.com>
2023-10-24 22:11:02 +0300

    Add groupBy tabs

--- generator/svelte/src/routes/+page.svelte
+++ generator/svelte/src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import GroupByTabs from '$lib/components/app/GroupByTabs.svelte';
 	import MasjidsList from '$lib/components/app/MasjidsList.svelte';
+	import { EGroupBy } from '$lib/constants';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 	let masjids = data.masjids;
 </script>
 
+<GroupByTabs groupBy={EGroupBy.Prayer} />
 <MasjidsList bind:masjids />
--- generator/svelte/src/routes/masjids/+page.svelte
+++ generator/svelte/src/routes/masjids/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import GroupByTabs from '$lib/components/app/GroupByTabs.svelte';
+	import { EGroupBy } from '$lib/constants';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
+
+<GroupByTabs groupBy={EGroupBy.Masjid} />
