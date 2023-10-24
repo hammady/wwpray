eb01372dbc20df31dd4efdad361593580ef16981
Fayez Nazzal <soulldev@gmail.com>
2023-10-24 07:31:18 +0300

    Fix using display name for subscribe

--- generator/svelte/src/routes/+page.svelte
+++ generator/svelte/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import MasjidsList from '$lib/components/app/MasjidsList.svelte';
+	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 	let masjids = data.masjids;
