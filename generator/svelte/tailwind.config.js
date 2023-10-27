fd00716a243bd07b91b7023ec02fa6c854f4c287
Fayez Nazzal <soulldev@gmail.com>
2023-10-27 11:43:23 +0300

    Add tailwind colors for color coding

--- generator/svelte/tailwind.config.js
+++ generator/svelte/tailwind.config.js
@@ -1,19 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      spacing: {
-        navbar: '6rem',
-      }
-    },
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("daisyui"),
-  ],
-  daisyui: {
-    themes: ["light"]
-  }
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			spacing: {
+				navbar: '6rem'
+			},
+			colors: {
+				danger: 'hsl(var(--danger) / <alpha-value>)'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				light: {
+					...require('daisyui/src/theming/themes')['[data-theme=light]'],
+					'--danger': '8 80% 56%'
+				}
+			}
+		]
+	}
+};
