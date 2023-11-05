a96243236e21d02817a06626762cb8f38fe79a64
Hossam Hammady <github@hammady.net>
2023-11-04 23:00:00 -0400

    Fix scraper tests after adding constructor param

--- scraper/test/test_tma.py
+++ scraper/test/test_tma.py
@@ -10,25 +10,25 @@ class TestTMA(TestCase):
     timezone = "America/Toronto"
 
     def test_find_nearest_day_when_day_is_in_list(self):
-        tma = TMA(TestTMA.timezone)
+        tma = TMA(TestTMA.timezone, masjid_id=0)
         self.assertEqual(tma._find_nearest_day(100, TestTMA.days), "One Hundred")
         self.assertEqual(tma._find_nearest_day(300, TestTMA.days), "Three Hundred")
 
     def test_find_nearest_day_when_day_is_between_two_existing_days(self):
-        tma = TMA(TestTMA.timezone)
+        tma = TMA(TestTMA.timezone, masjid_id=0)
         self.assertEqual(tma._find_nearest_day(200, TestTMA.days), "One Hundred")
 
     def test_find_nearest_day_when_day_is_smaller_than_smallest_day_in_list(self):
-        tma = TMA(TestTMA.timezone)
+        tma = TMA(TestTMA.timezone, masjid_id=0)
         self.assertEqual(tma._find_nearest_day(50, TestTMA.days), "Three Hundred")
 
     def test_find_nearest_day_when_day_is_larger_than_largest_day_in_list(self):
-        tma = TMA(TestTMA.timezone)
+        tma = TMA(TestTMA.timezone, masjid_id=0)
         self.assertEqual(tma._find_nearest_day(350, TestTMA.days), "Three Hundred")
 
     def test_find_nearest_day_when_day_is_not_in_list(self):
         days = {
             "400": "Four Hundred"
         }
-        tma = TMA(TestTMA.timezone)
+        tma = TMA(TestTMA.timezone, masjid_id=0)
         self.assertIsNone(tma._find_nearest_day(100, days))
