7c21426004be83c217ac722e150984042f20bd1d
Fayez Nazzal <soulldev@gmail.com>
2023-11-05 06:59:32 +0200

    Search on input

--- generator/svelte/src/lib/components/uikit/Navbar.svelte
+++ generator/svelte/src/lib/components/uikit/Navbar.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
 	import { APP_NAME } from '$lib/constants';
+	import { filteredMasjids, masjids } from '$lib/stores/masjids';
+	import { getFilteredMasjids } from '$lib/utils';
+	import type { FormEventHandler } from 'svelte/elements';
+
+	// When JS is enabled, search immediately on type
+	const onSearch: FormEventHandler<HTMLInputElement> = (event) => {
+		const search = (event.target as HTMLInputElement).value;
+
+		$filteredMasjids = getFilteredMasjids(search, $masjids);
+	};
 </script>
 
 <nav class="top-0 z-50 navbar bg-base-100 md:px-4 2xl:px-8 h-navbar shadow-md">
@@ -16,6 +27,7 @@
 					type="text"
 					placeholder="Search for masjid"
 					class="input input-bordered w-56 md:w-72"
+					on:input={onSearch}
 				/>
 			</div>
 		</form>
