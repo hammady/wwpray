ac2f0815fd2a2d5a35bd80045cea93edf40b38db
Fayez Nazzal <soulldev@gmail.com>
2023-11-05 06:59:16 +0200

    Move masjids to its own store

--- generator/svelte/src/routes/masjids/+page.svelte
+++ generator/svelte/src/routes/masjids/+page.svelte
@@ -2,11 +2,7 @@
 	import GroupByTabs from '$lib/components/app/GroupByTabs.svelte';
 	import MasjidsList from '$lib/components/app/MasjidsList.svelte';
 	import { EGroupBy } from '$lib/constants';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-	let masjids = data.masjids;
 </script>
 
 <GroupByTabs groupBy={EGroupBy.Masjid} />
-<MasjidsList bind:masjids />
+<MasjidsList />
