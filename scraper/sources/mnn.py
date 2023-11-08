b118beff3af107fa8b68396924792095f8bf8b2d
Hossam Hammady <github@hammady.net>
2023-11-07 21:58:50 -0500

    Scrape MNN directly
    Closes #75

--- scraper/sources/mnn.py
+++ scraper/sources/mnn.py
@@ -1,10 +1,29 @@
-from .bases import TMASource
+from .bases import HTMLSource
 
 
-class MNNexusSource(TMASource):
+class MNNexusSource(HTMLSource):
     def __init__(self):
-        super().__init__("America/Toronto", masjid_id=10)
-        self.name = "MNNexus"
+        super().__init__("MNNexus", headers={
+            "Accept": "text/html",
+        }, url="https://jangda.ca/mnnptcovid.php",
+        timezone="America/Toronto")
         self.display_name = 'Muslim Neighbour Nexus (MNN)'
         self.website = 'https://mnnexus.ca/'
         self.address = '3520 Odyssey Dr, Mississauga, ON L5M 0Y9'
+
+    def parse(self):
+        soup = super().parse()
+
+        tds = soup.select("body table tr td.time")
+        del tds[1] # remove sunrise
+        iqamas = self.generate_iqamas_output(
+            [value.text.strip() for value in tds]
+        )
+
+        jumas = [
+            f"MNN Masjid {juma.text.strip()}" for juma in soup.select("div.mnn-jumas div.juma")
+        ] + [
+            f"Churchill Meadows Comm Ctr {juma.text.strip()}" for juma in soup.select("div.comm-center-jumas div.juma")
+        ]
+
+        return iqamas, jumas
