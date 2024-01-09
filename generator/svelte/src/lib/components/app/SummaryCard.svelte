4b8d2a6e7883e2c5792c41d2a81deb2cd8d29b06
Fayez Nazzal <soulldev@gmail.com>
2024-01-09 07:03:43 +0200

    Enhance card spacing

--- generator/svelte/src/lib/components/app/SummaryCard.svelte
+++ generator/svelte/src/lib/components/app/SummaryCard.svelte
@@ -14,15 +14,16 @@
     const today = getCurrentLocalDateTime();
     const formattedToday = getFormattedDate(today);
     const todayName = getDayNameFromDate(today);
-    const sectionClasses = "flex flex-col gap-3 items-center p-5";
-    const cardClasses = "bg-bg01 shadow-card rounded-2xl";
+    const sectionClasses = "flex flex-col gap-3 items-center md:items-start p-5";
+    const cardClasses = "flex bg-bg01 shadow-card rounded-2xl";
     const cardMDClasses = "md:bg-bg01 md:shadow-card md:rounded-2xl";
     const mdResetClasses = "md:bg-transparent md:shadow-none md:rounded-none md:p-0"
+    const sectionHeaderClasses = "flex items-center gap-1"
 </script>
 
-<div class={tw(cardMDClasses, "flex md:items-center flex-col md:flex-row justify-between w-full p-8 lg:px-24 gap-8 md:gap-11", classes)} >
+<div class={tw(cardMDClasses, "flex flex-col md:flex-row justify-between w-full p-8 lg:px-24 gap-8 md:gap-11", classes)} >
     <div class={tw(sectionClasses, cardClasses, mdResetClasses)}>
-        <div class="flex items-center gap-1">
+        <div class={sectionHeaderClasses}>
             <CalendarIcon class="w-4 h-4" />
             <span class="text-xs">Today</span>
         </div>
@@ -37,11 +38,11 @@
     <div class="hidden divider divider-horizontal md:flex" />
 
     <div class={tw(sectionClasses, cardClasses, mdResetClasses)}>
-        <div class="flex items-center gap-1">
+        <div class={sectionHeaderClasses}>
             <PrayingIcon class="w-4 h-4" />
             <span class="text-xs">Next Prayer</span>
         </div>
-        <span class="mt-1 text-2xl font-semibold">
+        <span class="mt-1 text-2xl font-semibold capitalize">
             {nextPrayer.name}
         </span>
     </div>
@@ -49,7 +50,7 @@
     <div class="hidden divider divider-horizontal md:flex" />
 
     <div class={tw(sectionClasses, cardClasses, mdResetClasses)}>
-        <div class="flex items-center gap-1">
+        <div class={sectionHeaderClasses}>
             <ClockIcon class="w-4 h-4" />
             <span class="text-xs">Time remaining for next prayer</span>
         </div>
