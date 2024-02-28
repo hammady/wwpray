069fee7d985c35c5f9110aba1f93ce182b4dddff
Hossam Hammady <github@hammady.net>
2024-02-27 23:16:38 -0500

    Refactor WEIC source with more reliable selectors

--- scraper/sources/weic.py
+++ scraper/sources/weic.py
@@ -16,7 +16,7 @@ class WEICSource(HTMLSource):
     def parse(self):
         soup = super().parse()
 
-        tbody = soup.select_one("#post-1851 > div > div > div > div.et_pb_section.et_pb_section_1.et_section_regular > div.et_pb_with_border.et_pb_row.et_pb_row_0 > div > div.et_pb_with_border.et_pb_module.et_pb_text.et_pb_text_0.et_pb_text_align_left.et_pb_bg_layout_light > div > table > tbody > tr:nth-child(2) > td > b > table")
+        tbody = soup.select_one("table.dptTimetable")
         nested_rows = tbody.select_one("tr:nth-child(4)")
 
         zuhr_spans = nested_rows.select("tr")[0].select("td.jamah > span")
@@ -37,7 +37,8 @@ class WEICSource(HTMLSource):
             ]]
         )
 
-        tbody = soup.select_one("#post-1851 > div > div > div > div.et_pb_section.et_pb_section_1.et_section_regular > div.et_pb_with_border.et_pb_row.et_pb_row_0 > div > div.et_pb_with_border.et_pb_module.et_pb_text.et_pb_text_1.et_pb_text_align_left.et_pb_bg_layout_light > div > table")
+        # Get the tbody after the h1 containing "Friday"
+        tbody = soup.select_one("h1:-soup-contains('Friday')").find_next("table")
 
         # Get all trs in the tbody, and join the text of all tds
         jumas = [" - ".join([td.text.strip() for td in tr.select("td")]) for tr in tbody.select("tr")]
