b663c1ede4661f43c57d5e9c50d3ac42b1b434c4
Hossam Hammady <hossam@rayyan.ai>
2026-03-21 16:39:11 -0400

    Fix WEIC permanently for the Ramadan change

--- scraper/sources/weic.py
+++ scraper/sources/weic.py
@@ -17,25 +17,30 @@ class WEICSource(HTMLSource):
         soup = super().parse()
 
         tbody = soup.select_one("table.dptTimetable")
-        nested_rows = tbody.select_one("tr:nth-child(6)")
+        # In Ramadan 6 will succeed, otherwise 4 will succeed, so we try both
+        for start_row in [6, 4]:
+            try:
+                nested_rows = tbody.select_one(f"tr:nth-child({start_row})")
+                zuhr_spans = nested_rows.select("tr")[0].select("td.jamah > span")
+                if len(zuhr_spans) == 0:
+                    # today is not Friday, this is a single value
+                    zuhr_tag = nested_rows.select("tr")[0].select_one("td.jamah")
+                else:
+                    # today is Friday, jumas will be listed instead of Zuhr, take the first span
+                    zuhr_tag = zuhr_spans[0]
 
-        zuhr_spans = nested_rows.select("tr")[0].select("td.jamah > span")
-        if len(zuhr_spans) == 0:
-            # today is not Friday, this is a single value
-            zuhr_tag = nested_rows.select("tr")[0].select_one("td.jamah")
-        else:
-            # today is Friday, jumas will be listed instead of Zuhr, take the first span
-            zuhr_tag = zuhr_spans[0]
-
-        iqamas = self.generate_iqamas_output(
-            [value.text.strip() for value in [
-                tbody.select_one("tr:nth-child(5) > td.jamah"),
-                zuhr_tag,
-                nested_rows.select("tr")[1].select_one("td.jamah"),
-                nested_rows.select("tr")[2].select_one("td.jamah"),
-                nested_rows.select("tr")[3].select_one("td.jamah")
-            ]]
-        )
+                iqamas = self.generate_iqamas_output(
+                    [value.text.strip() for value in [
+                        tbody.select_one(f"tr:nth-child({start_row - 1}) > td.jamah"),
+                        zuhr_tag,
+                        nested_rows.select("tr")[1].select_one("td.jamah"),
+                        nested_rows.select("tr")[2].select_one("td.jamah"),
+                        nested_rows.select("tr")[3].select_one("td.jamah")
+                    ]]
+                )
+                break
+            except IndexError:
+                continue
 
         # Get the tbody after the h1 containing "Friday"
         tbody = soup.select_one("h1:-soup-contains('Friday')").find_next("table")
