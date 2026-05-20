a0114bba454e220ab7cfca2dacd17137586d0757
Hossam Hammady <hossam@rayyan.ai>
2026-05-20 19:58:34 -0400

    Support backward search in TMA parsing

--- scraper/sources/bases/tma.py
+++ scraper/sources/bases/tma.py
@@ -9,17 +9,23 @@ class TMASource(Source):
         timezone=timezone)
     
     @staticmethod
-    def _find_nearest_day(day_of_year, days):
-        # find the nearest day in the list of days
+    def _find_nearest_day_with_index(day_of_year, days):
+        # find the nearest day in the list of days, returning (day_number, day_data)
         started_on = day_of_year
         while True:
             if str(day_of_year) in days:
-                return days[str(day_of_year)]
+                return (day_of_year, days[str(day_of_year)])
             day_of_year -= 1
             if day_of_year < 1:
                 day_of_year = 366
             if day_of_year == started_on:
                 return None
+
+    @staticmethod
+    def _find_nearest_day(day_of_year, days):
+        # find the nearest day in the list of days
+        result = TMASource._find_nearest_day_with_index(day_of_year, days)
+        return result[1] if result is not None else None
 
     @staticmethod
     def _get_prayer_mapping(prayer):
@@ -35,15 +41,31 @@ class TMASource(Source):
         days = masjid["iqamas"]
         today = Source._get_current_time_in_timezone(self._timezone)
         day_of_year = today.timetuple().tm_yday
-        nearest_day = self._find_nearest_day(day_of_year, days)
-        if nearest_day is None:
+
+        collected = {}
+        search_from = day_of_year
+        visited_days = set()
+        while len(collected) < len(self._five_prayers):
+            result = self._find_nearest_day_with_index(search_from, days)
+            if result is None:
+                break
+            found_day, day_data = result
+            if found_day in visited_days:
+                break
+            visited_days.add(found_day)
+            for key in self._five_prayers:
+                mapped_key = self._get_prayer_mapping(key)
+                if key not in collected and mapped_key in day_data:
+                    collected[key] = day_data[mapped_key]
+            search_from = found_day - 1
+            if search_from < 1:
+                search_from = 366
+
+        if len(collected) < len(self._five_prayers):
             raise Exception("No iqamas found for source: " + self.name)
-        
+
         iqamas = self.generate_iqamas_output(
-            [
-                nearest_day[self._get_prayer_mapping(key)]
-                for key in self._five_prayers
-            ]
+            [collected[key] for key in self._five_prayers]
         )
 
         jumas = [f"{juma['timeDesc']} - {juma['locationDesc']}" for juma in masjid["jumas"]]
